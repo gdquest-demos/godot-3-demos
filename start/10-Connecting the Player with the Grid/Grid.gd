393c5d5cab0a35ddbc2aa970d5ecb551a2d313dd
Nathan <nathan@gdquest.com>
2017-06-01 11:30:19 +0200

    Add start project for grid-based example part 4

---
+++ start/10-Connecting the Player with the Grid/Grid.gd
@@ -0,0 +1,54 @@
+extends TileMap
+
+var tile_size = get_cell_size()
+var half_tile_size = tile_size / 2
+
+enum ENTITY_TYPES {PLAYER, OBSTACLE, COLLECTIBLE}
+
+var grid_size = Vector2(16,16)
+var grid = []
+
+onready var Obstacle = preload("res://Obstacle.tscn")
+
+
+func _ready():
+	for x in range(grid_size.x):
+		grid.append([])
+		for y in range(grid_size.y):
+			grid[x].append(null)
+	
+	var positions = []
+	for n in range(5):
+		var grid_pos = Vector2(randi() % int(grid_size.x), randi() % int(grid_size.y))
+		if not grid_pos in positions:
+			positions.append(grid_pos)
+	
+	for pos in positions:
+		var new_obstacle = Obstacle.instance()
+		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
+		grid[pos.x][pos.y] = OBSTACLE
+		add_child(new_obstacle)
+
+
+func is_cell_vacant(pos, direction):
+	var grid_pos = world_to_map(pos) + direction
+	
+	if grid_pos.x < grid_size.x and grid_pos.x >= 0:
+		if grid_pos.y < grid_size.y and grid_pos.y >= 0:
+			return true if grid[grid_pos.x][grid_pos.y] == null else false
+	return false
+
+
+func update_child_pos(child, new_pos, direction):
+	# Move a child to a new position in the grid Array
+	# Returns the new target world position of the child
+	var grid_pos = world_to_map(new_pos)
+	print(grid_pos)
+	grid[grid_pos.x][grid_pos.y] = null
+	
+	var new_grid_pos = grid_pos + direction
+	grid[new_grid_pos.x][new_grid_pos.y] = child.get_name()
+	
+	var target_pos = map_to_world(new_grid_pos) + half_tile_size
+	return target_pos
+
