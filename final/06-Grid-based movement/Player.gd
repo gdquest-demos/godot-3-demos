6e8c703659c351540782df5cfde3e351b82c4c22
Nathan <nathan@gdquest.com>
2017-06-01 08:46:57 +0200

    Refactor player grid-based movement

--- final/06-Grid-based movement/Player.gd
+++ final/06-Grid-based movement/Player.gd
@@ -1,10 +1,5 @@
 extends KinematicBody2D
 
-const TOP	= Vector2(0, -1)
-const RIGHT	= Vector2(1, 0)
-const DOWN	= Vector2(0, 1)
-const LEFT	= Vector2(-1, 0)
-
 var direction = Vector2()
 
 const MAX_SPEED = 400
@@ -13,6 +8,7 @@ var speed = 0
 var velocity = Vector2()
 
 var target_pos = Vector2()
+var target_direction = Vector2()
 var is_moving = false
 
 onready var grid = get_parent()
@@ -22,36 +18,35 @@ func _ready():
 
 
 func _fixed_process(delta):
-	var player_input = Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")
+	direction = Vector2()
+	speed = 0
 
-	if player_input and not is_moving:
-		if Input.is_action_pressed("move_up"):
-			direction = TOP
-		elif Input.is_action_pressed("move_right"):
-			direction = RIGHT
-		elif Input.is_action_pressed("move_down"):
-			direction = DOWN
-		elif Input.is_action_pressed("move_left"):
-			direction = LEFT
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("move_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("move_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("move_down"):
+		direction.y = 1
 
+	if not is_moving and direction != Vector2():
+		target_direction = direction
 		if grid.is_cell_vacant(get_pos(), direction):
 			target_pos = grid.update_child_pos(self, get_pos(), direction)
 			is_moving = true
+	elif is_moving:
+		speed = MAX_SPEED
+		velocity = speed * target_direction * delta
 
-	speed = MAX_SPEED if is_moving else 0
-	velocity = speed * direction * delta
-
-	if target_pos != Vector2():
 		var pos = get_pos()
 		var distance_to_target = Vector2(abs(target_pos.x - pos.x), abs(target_pos.y - pos.y))
 
 		if abs(velocity.x) > distance_to_target.x:
-			velocity.x = direction.x * distance_to_target.x
+			velocity.x = target_direction.x * distance_to_target.x
 			is_moving = false
-			target_pos = Vector2()
 		if abs(velocity.y) > distance_to_target.y:
-			velocity.y = direction.y * distance_to_target.y
+			velocity.y = target_direction.y * distance_to_target.y
 			is_moving = false
-			target_pos = Vector2()
 
 		move(velocity)
