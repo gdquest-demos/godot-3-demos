5c045e371c75d2bbcbe59f3aa59b18b1f12de8e6
Nathan <nathan@gdquest.com>
2017-06-03 07:26:20 +0200

    Simplify Player movement code
    - Remove ref to node in Grid.update_child_pos
    - add Grid.ENTITY_TYPES enum

--- final/06-Grid-based movement/Grid.gd
+++ final/06-Grid-based movement/Grid.gd
@@ -1,6 +1,8 @@
 # Collection of functions to work with a Grid. Stores all its children in the grid array
 extends TileMap
 
+enum ENTITY_TYPES {PLAYER, OBSTACLE, COLLECTIBLE}
+
 var tile_size = get_cell_size()
 var half_tile_size = tile_size / 2
 var grid_size = Vector2(16, 16)
@@ -51,14 +53,14 @@ func is_cell_vacant(pos=Vector2(), direction=Vector2()):
 	return false
 
 
-func update_child_pos(child, new_pos, direction):
+func update_child_pos(new_pos, direction, type):
 	# Remove node from current cell, add it to the new cell, returns the new target move_to position
 	var grid_pos = world_to_map(new_pos)
 	print(grid_pos)
 	grid[grid_pos.x][grid_pos.y] = null
 	
 	var new_grid_pos = grid_pos + direction
-	grid[new_grid_pos.x][new_grid_pos.y] = child.get_name()
+	grid[new_grid_pos.x][new_grid_pos.y] = type
 	
 	var target_pos = map_to_world(new_grid_pos) + half_tile_size
 	return target_pos
--- final/06-Grid-based movement/Player.gd
+++ final/06-Grid-based movement/Player.gd
@@ -11,9 +11,13 @@ var target_pos = Vector2()
 var target_direction = Vector2()
 var is_moving = false
 
-onready var grid = get_parent()
+var type
+var grid
+
 
 func _ready():
+	grid = get_parent()
+	type = grid.PLAYER
 	set_fixed_process(true)
 
 
@@ -21,32 +25,31 @@ func _fixed_process(delta):
 	direction = Vector2()
 	speed = 0
 
-	if Input.is_action_pressed("move_left"):
-		direction.x = -1
-	elif Input.is_action_pressed("move_right"):
-		direction.x = 1
-	elif Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):
 		direction.y = -1
 	elif Input.is_action_pressed("move_down"):
 		direction.y = 1
 
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("move_right"):
+		direction.x = 1
+
 	if not is_moving and direction != Vector2():
 		target_direction = direction
 		if grid.is_cell_vacant(get_pos(), direction):
-			target_pos = grid.update_child_pos(self, get_pos(), direction)
+			target_pos = grid.update_child_pos(get_pos(), direction, type)
 			is_moving = true
 	elif is_moving:
 		speed = MAX_SPEED
 		velocity = speed * target_direction * delta
 
 		var pos = get_pos()
-		var distance_to_target = Vector2(abs(target_pos.x - pos.x), abs(target_pos.y - pos.y))
+		var distance_to_target = pos.distance_to(target_pos)
+		var move_distance = velocity.length()
 
-		if abs(velocity.x) > distance_to_target.x:
-			velocity.x = target_direction.x * distance_to_target.x
-			is_moving = false
-		if abs(velocity.y) > distance_to_target.y:
-			velocity.y = target_direction.y * distance_to_target.y
+		if move_distance > distance_to_target:
+			velocity = target_direction.normalized() * distance_to_target
 			is_moving = false
 
 		move(velocity)
