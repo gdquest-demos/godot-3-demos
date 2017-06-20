f0a5bca0b50d4c87878229d760efb5c5c7321e4a
Nathan Lovato <nathan.lovato.art@gmail.com>
2017-06-20 21:30:13 +0200

    First commit

---
+++ Sho-pong/Player/PlayerPaddle.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
