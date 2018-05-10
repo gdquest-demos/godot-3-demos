72032edf6810e5fbabcaaf0a14ca9db1d9954fc8
Nathan <nathan@gdquest.com>
2018-05-10 23:05:37 +0900

    Add re-designed GUI demo with programmable bars

---
+++ 2018/05-10-game-user-interface/end/interface/bar/Bar.gd
@@ -0,0 +1,27 @@
+tool
+extends HBoxContainer
+
+export(int) var current_value = 12 setget set_current_value
+export(int) var minimum_value = 0 setget set_minimum_value
+export(int) var maximum_value = 34 setget set_maximum_value
+
+
+func set_current_value(value):
+	current_value = value
+	$TextureProgress.value = value
+	update_count_text()
+
+
+func set_minimum_value(value):
+	minimum_value = value
+	$TextureProgress.min_value = value
+
+
+func set_maximum_value(value):
+	maximum_value = value
+	$TextureProgress.max_value = value
+	update_count_text()
+
+
+func update_count_text():
+	$Count/Background/Number.text = str(current_value) + '/' + str(maximum_value)
