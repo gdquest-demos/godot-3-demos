d5dfb469fa67a010e407bac2e9a108c4499221d4
Nathan Lovato <nathan@gdquest.com>
2018-06-20 15:29:00 +0900

    Add example with new reusable state_machine node
    Decouples the state machine from the KinematicBody2D/Actor

---
+++ 2018/04-24-finite-state-machine/player_v2/player_state_machine.gd
@@ -0,0 +1,32 @@
+extends "res://player_v2/state_machine.gd"
+
+func _ready():
+	states_map = {
+		"idle": $Idle,
+		"move": $Move,
+		"jump": $Jump,
+		"stagger": $Stagger,
+		"attack": $Attack,
+	}
+
+func _change_state(state_name):
+	"""
+	The base state_machine interface this node extends does most of the work
+	"""
+	if not _active:
+		return
+	if state_name in ["stagger", "jump", "attack"]:
+		states_stack.push_front(states_map[state_name])
+	._change_state(state_name)
+
+func _input(event):
+	"""
+	Here we only handle input that can interrupt states, attacking in this case
+	otherwise we let the state node handle it
+	"""
+	if event.is_action_pressed("attack"):
+		if current_state == $Attack:
+			return
+		_change_state("attack")
+		return
+	current_state.handle_input(event)
