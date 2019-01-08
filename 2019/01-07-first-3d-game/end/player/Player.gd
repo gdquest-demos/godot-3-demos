30ec4368673c7c1deb273d9398d868c8abde7e15
Nathan Lovato <nathan@gdquest.com>
2019-01-08 10:05:29 +0900

    3d maze: clarify player movement code

--- 2019/01-07-first-3d-game/end/player/Player.gd
+++ 2019/01-07-first-3d-game/end/player/Player.gd
@@ -1,14 +1,15 @@
 extends KinematicBody
 class_name Player
 
-export var move_speed : = 10.0
-export var rotate_speed : = 3.0 
-export var jump_force : = 50.0
+export var move_speed : float = 10.0
+export var rotate_speed : float = 3.0
+export var jump_force : float = 50.0
 
 func _physics_process(delta : float) -> void:
-	var z_movement = int(Input.is_action_pressed("backward")) - int(Input.is_action_pressed("forward"))
-	var rotate = int(Input.is_action_pressed("left")) - int(Input.is_action_pressed("right"))
-	
-	rotation.y += rotate * rotate_speed * delta
-	
-	move_and_collide(Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), rotation.y) * z_movement * delta * move_speed)
+  var z_movement : float = Input.get_action_strength("backward") - Input.get_action_strength("forward")
+  var rotate : float = Input.get_action_strength("left") - Input.get_action_strength("right")
+
+  rotation.y += rotate * rotate_speed * delta
+  var direction : Vector3 = Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), rotation.y)
+  var velocity : Vector3 = direction * z_movement * delta * move_speed
+  move_and_collide(velocity)
