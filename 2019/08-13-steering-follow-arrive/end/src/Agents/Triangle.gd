64394a0a4b43797715c23f42dee676d5fbe36a40
Nathan Lovato <nathan@gdquest.com>
2019-08-13 21:05:20 +0200

    Add Godot 3.1 Steering arrive end demo

---
+++ 2019/08-13-steering-follow-arrive/end/src/Agents/Triangle.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+
+onready var sprite: Sprite = $TriangleRed
+
+const DISTANCE_THRESHOLD: = 3.0
+
+export var max_speed: = 500.0
+
+var target_global_position: = Vector2.ZERO setget set_target_global_position
+var _velocity: = Vector2.ZERO
+
+var _is_following: = false
+
+
+func _ready() -> void:
+	set_physics_process(false)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		self.target_global_position = get_global_mouse_position()
+
+
+func _physics_process(delta: float) -> void:
+	if global_position.distance_to(target_global_position) < DISTANCE_THRESHOLD:
+		set_physics_process(false)
+	_velocity = Steering.arrive_to(
+		_velocity,
+		global_position,
+		target_global_position,
+		max_speed
+	)
+	_velocity = move_and_slide(_velocity)
+	sprite.rotation = _velocity.angle()
+
+
+func set_target_global_position(value: Vector2) -> void:
+	target_global_position = value
+	set_physics_process(true)
